681082462743f2ee1f67455518bda69620f8bd24
GAMESYS\vlatko.dimeski <vlatko.dimeski@ballys.com>
2024-08-21 22:48:10 +0200

    commit

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,55 +1,55 @@
-// import { gamdom } from "../fixtures/gamdom.fixture";
-// import { expect } from "@playwright/test";
-// import { baseUrl, demogmEndpoints } from "../defaults/config.enums";
-// import { basicHeaders } from "../data/headersData";
-// import { editIssuePayload } from "../data/payloadData";
+import { gamdom } from "../fixtures/gamdom.fixture";
+import { expect } from "@playwright/test";
+import { baseUrl, demogmEndpoints } from "../defaults/config.enums";
+import { basicHeaders } from "../data/headersData";
+import { editIssuePayload } from "../data/payloadData";
 
-// gamdom.describe("api fixture", () => {
-//   let issueId: string;
+gamdom.describe("api fixture", () => {
+  let issueId: string;
 
-//   gamdom("Get issue by id; @get @api", async ({ request, api }) => {
-//     const createJiraIssueResponse = await api.postIssue()
-//     const createJiraIssueResponseBody = await createJiraIssueResponse.json();
-//     issueId = createJiraIssueResponseBody.id;
+  gamdom("Get issue by id; @get @api", async ({ request, api }) => {
+    const createJiraIssueResponse = await api.postIssue()
+    const createJiraIssueResponseBody = await createJiraIssueResponse.json();
+    issueId = createJiraIssueResponseBody.id;
 
-//     const getIssueByIdResponse = await request.get(`${baseUrl.Api}${demogmEndpoints.getSpecificIssue}${issueId}`,{
-//         headers: basicHeaders,
-//       }
-//     );
-//     const getIssueByIdResponseBody = await getIssueByIdResponse.json();
-//     expect(getIssueByIdResponse.status()).toBe(200);
-//     expect(getIssueByIdResponseBody).toHaveProperty("id", issueId);
-//   });
-//   gamdom("Post issue; @post @api", async ({ api }) => {
-//     const createJiraIssueResponse = await api.postIssue()
-//     const createJiraIssueResponseBody = await createJiraIssueResponse.json();
-//     issueId = createJiraIssueResponseBody.id;
+    const getIssueByIdResponse = await request.get(`${baseUrl.Api}${demogmEndpoints.getSpecificIssue}${issueId}`,{
+        headers: basicHeaders,
+      }
+    );
+    const getIssueByIdResponseBody = await getIssueByIdResponse.json();
+    expect(getIssueByIdResponse.status()).toBe(200);
+    expect(getIssueByIdResponseBody).toHaveProperty("id", issueId);
+  });
+  gamdom.skip("Post issue; @post @api", async ({ api }) => {
+    const createJiraIssueResponse = await api.postIssue()
+    const createJiraIssueResponseBody = await createJiraIssueResponse.json();
+    issueId = createJiraIssueResponseBody.id;
 
-//     expect(createJiraIssueResponse.status()).toBe(201);
-//     expect(createJiraIssueResponseBody).toHaveProperty("id", issueId);
-//     expect(createJiraIssueResponseBody.key).toMatch(/^[A-Z]+-\d+$/);
-//   });
-//   gamdom("Edit issue; @edit @api", async ({ api, request }, worker) => {
-//     const createJiraIssueResponse = await api.postIssue()
-//     const createJiraIssueResponseBody = await createJiraIssueResponse.json();
-//     issueId = createJiraIssueResponseBody.id;
+    expect(createJiraIssueResponse.status()).toBe(201);
+    expect(createJiraIssueResponseBody).toHaveProperty("id", issueId);
+    expect(createJiraIssueResponseBody.key).toMatch(/^[A-Z]+-\d+$/);
+  });
+  gamdom.skip("Edit issue; @edit @api", async ({ api, request }, worker) => {
+    const createJiraIssueResponse = await api.postIssue()
+    const createJiraIssueResponseBody = await createJiraIssueResponse.json();
+    issueId = createJiraIssueResponseBody.id;
 
-//     const editIssueResponse = await request.put(`${baseUrl.Api}${demogmEndpoints.editIssue}${issueId}`,{
-//       headers: basicHeaders,
-//       data: editIssuePayload
-//     }
-//    );
-//     expect(editIssueResponse.status()).toBe(204);
-//   });
-//   gamdom("Delete issue; @delete @api", async ({ api, request }) => {
-//     const createJiraIssueResponse = await api.postIssue()
-//     const createJiraIssueResponseBody = await createJiraIssueResponse.json();
-//     issueId = createJiraIssueResponseBody.id;
+    const editIssueResponse = await request.put(`${baseUrl.Api}${demogmEndpoints.editIssue}${issueId}`,{
+      headers: basicHeaders,
+      data: editIssuePayload
+    }
+   );
+    expect(editIssueResponse.status()).toBe(204);
+  });
+  gamdom.skip("Delete issue; @delete @api", async ({ api, request }) => {
+    const createJiraIssueResponse = await api.postIssue()
+    const createJiraIssueResponseBody = await createJiraIssueResponse.json();
+    issueId = createJiraIssueResponseBody.id;
     
-//     const deleteIssueResponse = await request.delete(`${baseUrl.Api}${demogmEndpoints.deleteIssue}${issueId}`,{
-//       headers: basicHeaders,
-//     }
-//    );
-//     expect(deleteIssueResponse.status()).toBe(204);
-//   });
-// });
+    const deleteIssueResponse = await request.delete(`${baseUrl.Api}${demogmEndpoints.deleteIssue}${issueId}`,{
+      headers: basicHeaders,
+    }
+   );
+    expect(deleteIssueResponse.status()).toBe(204);
+  });
+});
--- tests/ui.spec.ts
+++ tests/ui.spec.ts
@@ -1,48 +1,48 @@
-import { gamdom } from "../fixtures/gamdom.fixture";
-import { expect } from "@playwright/test";
-import { baseUrl, loginDetails } from "../defaults/config.enums";
+// import { gamdom } from "../fixtures/gamdom.fixture";
+// import { expect } from "@playwright/test";
+// import { baseUrl, loginDetails } from "../defaults/config.enums";
 
-gamdom.describe("ui fixture", () => {
-  let gameToPlay: string = 'Coins Of Alkemor';
+// gamdom.describe("ui fixture", () => {
+//   let gameToPlay: string = 'Coins Of Alkemor';
   
-  // gamdom.use({
-  //   geolocation: { longitude: 14.3754, latitude: 35.9375 },
-  //   permissions: ['geolocation'],
-  // });
-  gamdom.beforeEach(async ({ registration, context }) => {
-    //await context.setGeolocation({ longitude: 14.3754, latitude: 35.9375 });
-    await registration.navigateToWebUi(baseUrl.Ui);
-  });
+//   // gamdom.use({
+//   //   geolocation: { longitude: 14.3754, latitude: 35.9375 },
+//   //   permissions: ['geolocation'],
+//   // });
+//   gamdom.beforeEach(async ({ registration, context }) => {
+//     //await context.setGeolocation({ longitude: 14.3754, latitude: 35.9375 });
+//     await registration.navigateToWebUi(baseUrl.Ui);
+//   });
 
-  gamdom('Player can register', async ({ registration }) => {
-    await registration.registerButtonClick();
-    await registration.closePopUpNotification();
-    await registration.populatePlayerDetails();
-    await registration.agreeTAndcClick();
-    await registration.startPlayingButtonClick();
-    await registration.expectcloudFlareInterceptorToBeVisible();
-});
+//   gamdom('Player can register', async ({ registration }) => {
+//     await registration.registerButtonClick();
+//     await registration.closePopUpNotification();
+//     await registration.populatePlayerDetails();
+//     await registration.agreeTAndcClick();
+//     await registration.startPlayingButtonClick();
+//     await registration.expectcloudFlareInterceptorToBeVisible();
+// });
 
-  gamdom.skip('Player can login to the Ui with valid credentials', async ({ login, registration }) => {
-    await login.clickSignInButton();
-    await registration.closePopUpNotification();
-    await login.populateSignInDetails(loginDetails.userName, loginDetails.passWord);
-    await login.startPlayingSignInButtonClick();
-    await login.expectcloudFlareLoginInterceptorToBeVisible();
-});
+//   gamdom.skip('Player can login to the Ui with valid credentials', async ({ login, registration }) => {
+//     await login.clickSignInButton();
+//     await registration.closePopUpNotification();
+//     await login.populateSignInDetails(loginDetails.userName, loginDetails.passWord);
+//     await login.startPlayingSignInButtonClick();
+//     await login.expectcloudFlareLoginInterceptorToBeVisible();
+// });
   
-  gamdom.skip('Player can open game while logged out', async ({ game }) => {
-    await game.casinoClick();
-    await game.searchGame(gameToPlay)
-    await game.selectGameForPlay()
-    await game.expectGameplayComponentToBeVisible();
-});
+//   gamdom.skip('Player can open game while logged out', async ({ game }) => {
+//     await game.casinoClick();
+//     await game.searchGame(gameToPlay)
+//     await game.selectGameForPlay()
+//     await game.expectGameplayComponentToBeVisible();
+// });
 
 
 
 
 
 
-});
+// });
 
 
